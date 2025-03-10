4ac62cef671811a25732da26fc0e2cf29e718ff1
K Sadov <ksadov@gmail.com>
2025-03-10 17:58:29 -0500

    allow taking snapshot from start

--- src/backtesting/dataset.py
+++ src/backtesting/dataset.py
@@ -12,20 +12,32 @@ from src.agent import MarketPrediction
 
 
 def make_example(
-    question,
-    description,
-    creatorUsername,
-    resolution,
-    tradeHistory,
-    comments,
+    question: str,
+    description: str,
+    creatorUsername: str,
+    resolution: str,
+    tradeHistory: list[dict],
+    comments: list[dict],
+    timestamp: Optional[int] = None,
 ):
-    tradeHistory = json.loads(tradeHistory)
     comments = json.loads(comments)
-    random_snapshot = tradeHistory[math.floor(random.random() * len(tradeHistory))]
-    timestamp, probability = (
-        random_snapshot["snapshotTime"],
-        random_snapshot["probability"],
-    )
+    if timestamp is None:
+        random_snapshot = tradeHistory[math.floor(random.random() * len(tradeHistory))]
+        timestamp, probability = (
+            random_snapshot["snapshotTime"],
+            random_snapshot["probability"],
+        )
+    else:
+        probability = 0.5
+        most_recent_time = tradeHistory[0]["snapshotTime"]
+        for trade in tradeHistory:
+            if (
+                trade["snapshotTime"] <= timestamp
+                and trade["snapshotTime"] > most_recent_time
+            ):
+                most_recent_time = trade["snapshotTime"]
+                probability = trade["probability"]
+
     comments_pre_snapshot = [
         comment for comment in comments if comment["createdTime"] < timestamp
     ]
@@ -57,6 +69,8 @@ def can_use(
     groupSlugs: Iterable[str],
     cutoff_time: datetime.datetime,
     exclude_groups: Iterable[str],
+    trade_history: list[dict],
+    min_num_trades: Optional[int] = None,
 ) -> bool:
     cutoff_time = cutoff_time.timestamp() * 1000
     if createdTime < cutoff_time:
@@ -64,6 +78,8 @@ def can_use(
     for exclude_group in exclude_groups:
         if exclude_group in groupSlugs:
             return False
+    if min_num_trades is not None and len(trade_history) < min_num_trades:
+        return False
     return True
 
 
@@ -71,27 +87,37 @@ def load_examples(
     parquet_path: Path,
     cutoff_date: Optional[datetime.datetime],
     exclude_groups: Iterable[str],
+    trade_from_start: bool,
     max_examples: Optional[int] = None,
+    min_num_trades: Optional[int] = None,
 ):
     df = pd.read_parquet(parquet_path)
     examples = []
     for i, row in df.iterrows():
         if max_examples is not None and len(examples) >= max_examples:
             break
+        trade_history = json.loads(row["tradeHistory"])
         if can_use(
             row["createdTime"],
             row["groupSlugs"],
-            (cutoff_date if cutoff_date else datetime.datetime.now()),
+            cutoff_date,
             exclude_groups,
+            trade_history,
+            min_num_trades,
         ):
+            if trade_from_start:
+                timestamp = row["createdTime"]
+            else:
+                timestamp = None
             examples.append(
                 make_example(
                     row["question"],
                     row["description"],
                     row["creatorUsername"],
                     row["resolution"],
-                    row["tradeHistory"],
+                    trade_history,
                     row["comments"],
+                    timestamp,
                 )
             )
     return examples
@@ -99,22 +125,30 @@ def load_examples(
 
 def test(parquet_path):
     df = pd.read_parquet(parquet_path)
-    representative_entry = df.iloc[50123]
+    representative_entry = df.iloc[-10]
     print("Parquet entry:")
     print(representative_entry)
     cutoff_time_string = "2022-02-01"
-    cutoff_time = datetime.datetime.strptime(cutoff_time_string, "%Y-%m-%d").timestamp()
+    cutoff_time = datetime.datetime.strptime(cutoff_time_string, "%Y-%m-%d")
     exclude_groups = ["sex-and-love"]
-    dataset = load_examples(parquet_path, cutoff_time, exclude_groups, max_examples=5)
+    trade_from_start = True
+    max_examples = 3
+    min_num_trades = 10
+    dataset = load_examples(
+        parquet_path,
+        cutoff_time,
+        exclude_groups,
+        trade_from_start,
+        max_examples=max_examples,
+        min_num_trades=min_num_trades,
+    )
     print("Dataset length:", len(dataset))
     for i in range(len(dataset)):
         print(dataset[i])
 
 
 def main():
-    parquet_path = Path(
-        "/Users/ksadov/Documents/manifold2025/bot/bot_dspy/manifold_dataset.parquet"
-    )
+    parquet_path = Path("processed_data/test.parquet")
     test(parquet_path)
 
 
