73a103e5046049ad518e92584190f3ec1051dc79
K Sadov <ksadov@gmail.com>
2025-03-12 15:51:29 -0500

    use brier for optimizer

--- src/scripts/optimize.py
+++ src/scripts/optimize.py
@@ -1,13 +1,19 @@
 import argparse
 
+from src.scripts import trade
 import dspy
 from typing import Optional
 from pathlib import Path
-from src.evaluation import setup_pipeline, soft_cross_entropy, validate_directional
+from src.evaluation import (
+    setup_pipeline,
+    soft_cross_entropy,
+    validate_directional,
+    brier_score,
+)
 from src.backtesting.dataset import load_examples
 
 
-def metric_for_optimizer(example, pred, trace=None):
+def optimizer_cross_entropy(example, pred, trace=None):
     """
     via https://github.com/stanfordnlp/dspy/issues/1978
     If we're "bootstrapping" for optimization, return a boolean value
@@ -22,6 +28,17 @@ def metric_for_optimizer(example, pred, trace=None):
         return score
 
 
+def optimizer_brier(example, pred, trace=None):
+    if trace is not None:
+        directional_value = validate_directional(example, pred, trace)
+        print(f"Directional value: {directional_value}")
+        return directional_value > 0
+    else:
+        score = brier_score(example, pred, trace)
+        print(f"Brier score: {score}")
+        return score
+
+
 def optimize(
     config_path: Path,
     train_parquet_path: Path,
@@ -32,6 +49,8 @@ def optimize(
     save_filename: Path,
     log_level: str,
     optimizer: str,
+    trade_from_start: bool,
+    use_brier: bool,
 ):
     predict_market, _, _, cutoff_date, exclude_groups = setup_pipeline(
         config_path,
@@ -42,17 +61,21 @@ def optimize(
         train_parquet_path,
         cutoff_date,
         exclude_groups,
+        trade_from_start,
+        use_brier,
         max_train_examples,
     )
     valset = load_examples(
         val_parquet_path,
         cutoff_date,
         exclude_groups,
+        trade_from_start,
+        use_brier,
         max_val_examples,
     )
     if optimizer == "MIPROv2":
         tp = dspy.MIPROv2(
-            metric=metric_for_optimizer,
+            metric=optimizer_brier if use_brier else optimizer_cross_entropy,
             auto="light",
             num_threads=num_threads,
         )
@@ -65,7 +88,7 @@ def optimize(
         )
     elif optimizer == "COPRO":
         tp = dspy.COPRO(
-            metric=metric_for_optimizer,
+            metric=optimizer_brier if use_brier else optimizer_cross_entropy,
             verbose=True,
         )
         kwargs = dict(num_threads=num_threads, display_progress=True, display_table=0)
@@ -96,7 +119,17 @@ def main():
     parser.add_argument("--save_filename", type=Path, required=True)
     parser.add_argument("--log_level", type=str, default="INFO")
     parser.add_argument("--optimizer", type=str, default="MIPROv2")
+    parser.add_argument("--random_snapshot", action="store_true")
+    parser.add_argument("--score_type", type=str, default="brier")
     args = parser.parse_args()
+    assert args.optimizer in [
+        "MIPROv2",
+        "COPRO",
+    ], f"Unknown optimizer: {args.optimizer}"
+    assert args.score_type in [
+        "brier",
+        "soft_cross_entropy",
+    ], f"Unknown score type: {args.score_type}"
     optimize(
         args.config_path,
         args.train_parquet_path,
@@ -107,6 +140,8 @@ def main():
         args.save_filename,
         args.log_level,
         args.optimizer,
+        not args.random_snapshot,
+        args.score_type == "brier",
     )
 
 
